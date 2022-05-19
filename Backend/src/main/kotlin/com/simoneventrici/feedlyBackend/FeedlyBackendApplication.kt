e9e52dc31b43f19c677586a438e993c007ae8a75
simoven <simo10@live.it>
2022-05-19 19:37:20 +0200

    First commit

---
+++ Backend/src/main/kotlin/com/simoneventrici/feedlyBackend/FeedlyBackendApplication.kt
@@ -0,0 +1,18 @@
+package com.simoneventrici.feedlyBackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.web.client.RestTemplate
+
+@SpringBootApplication
+class FeedlyBackendApplication {
+
+	@Bean
+	fun restTemplate(restTemplateBuilder: RestTemplateBuilder): RestTemplate = restTemplateBuilder.build()
+}
+
+fun main(args: Array<String>) {
+	runApplication<FeedlyBackendApplication>(*args)
+}
---
+++ Backend/src/test/kotlin/com/simoneventrici/feedlyBackend/FeedlyBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.simoneventrici.feedlyBackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FeedlyBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
